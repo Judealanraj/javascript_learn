5d1ec2d6f6be4930de7e3b30015dfb6a1e1ea751
Jude Alan <judealanraj1@gmail.com>
2025-02-08 18:20:27 +0530

    today's learning

--- js_files/script.js
+++ js_files/script.js
@@ -24,3 +24,19 @@ ar_fu("judeww");
 
 let age= prompt("enter your age");
 console.log(age);
+
+let f_name = "jude"
+let ta = `<table><td>"name"</td><td>${f_name}</td></table>`
+document.body.innerHTML = ta;
+
+//array of 10 objects with different names  and different ages
+let students = [
+    {name:"jude", age:23},
+    {name:"jude", age:23},
+    {name:"jude", age:23}]
+
+
+let person = new Object();
+person.name = "jude";
+person.age = 23;
+console.table(person);
